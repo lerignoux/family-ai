d9b911a3c489123147435dc8e7349b4be43e8017
Laurent Erignoux <lerignoux@gmail.com>
2025-08-10 17:32:08 +0800

    Adding a direct audio translation route in argos-translate service

--- argos-translate/main.py
+++ argos-translate/main.py
@@ -1,12 +1,15 @@
+import aiofiles
 import logging
 import os
+import requests
 import sys
 import torch
 from bson import ObjectId
 
 import argostranslate.package
 import argostranslate.translate
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
@@ -55,3 +58,37 @@ def translate(sentence: str, from_code='en', to_code='fr'):
     """
     translatedText = argostranslate.translate.translate(sentence, from_code, to_code)
     return {"result": translatedText}
+
+
+@app.post("/translate_audio")
+async def translate_audio(file: UploadFile, from_code='en', to_code='fr'):
+    temp_file = f"/argos-translate/input/input_{ObjectId()}_{file.filename}"
+    """
+    Process the given input into audio convert in mp3 and returns it as a file.
+    """
+    async with aiofiles.open(temp_file, 'wb') as out_file:
+        content = await file.read()
+        await out_file.write(content)
+
+    multipart_form_data = {
+        'file': content,
+        'language': from_code
+    }
+    response = requests.post("http://192.168.2.10:8186/stt", files=multipart_form_data)
+    data = response.json()
+    translatedText = argostranslate.translate.translate(data.get('result'), from_code, to_code)
+
+
+    post_data = {
+        'sentence': translatedText,
+        'model': 'kokoro-82M',
+        'language': to_code
+    }
+    response = requests.post("http://192.168.2.10:8186/tts", json=post_data)
+    if response.status_code == 200:
+        temp_file = f"/argos-translate/output/output_{ObjectId()}_{file.filename}"
+        with open(temp_file, 'wb') as f:
+            f.write(response.content)
+    else:
+        raise Exception(f"Failed translating audio file tts response {response.status_code}")
+    return FileResponse(temp_file, media_type="audio/mpeg")
